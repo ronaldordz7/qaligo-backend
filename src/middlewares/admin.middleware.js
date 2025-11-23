a245793fd158de845610f55ee2ad8e51ca24bed3
ronaldordz7 <ronaldorodriguezacevedo7@gmail.com>
2025-11-23 17:46:18 -0500

    error 2

--- src/middlewares/admin.middleware.js
+++ src/middlewares/admin.middleware.js
@@ -1,13 +1,8 @@
-function adminMiddleware(req, res, next) {
-  if (!req.user) {
-    return res.status(401).json({ message: 'No autenticado' });
+function requireAdmin(req, res, next) {
+  if (!req.user || req.user.role !== "admin") {
+    return res.status(403).json({ message: "Acceso denegado: Se requiere rol admin." });
   }
-
-  if (req.user.role !== 'admin') {
-    return res.status(403).json({ message: 'Acceso denegado: se requiere rol admin' });
-  }
-
   next();
 }
 
-module.exports = adminMiddleware;
+module.exports = requireAdmin;

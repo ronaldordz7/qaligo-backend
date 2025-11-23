6ae738fd6324a16098e7d594f9c121aab7192f81
ronaldordz7 <ronaldorodriguezacevedo7@gmail.com>
2025-11-23 17:16:19 -0500

    error 1

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -2,16 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 const orderController = require('../controllers/order.controller');
-
+const requireAuth = require('../middlewares/auth.middleware');
+const requireAdmin = require('../middlewares/admin.middleware');
 // Crear una orden
 router.post('/', orderController.createOrder);
 
 // Obtener órdenes por usuario
 router.get('/:userId', orderController.getOrdersByUser);
 
-module.exports = router;
+
 
 router.get('/admin/all', requireAuth, requireAdmin, orderController.getAllOrders);
 
 router.put('/admin/status/:orderId', requireAuth, requireAdmin, orderController.updateOrderStatus);
 
+module.exports = router;
